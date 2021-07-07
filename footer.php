afcfe869644ba7f6ce7feba58f386b7821f8e408
manish-karki63 <manish.karki33@gmail.com>
2021-07-08 01:38:47 +0545

    First Commit

---
+++ footer.php
@@ -0,0 +1,26 @@
+<!-- Footer Section -->
+        <footer>
+            <div class="container">
+                <ul class="footer-widget clearfix">
+                    <li>
+                        <span class="icon"><i class="fa fa-map-marker"></i></span>
+                        <span class="caption">Old Baneshwor, Basuki Marga</span>
+                    </li>
+                    <li>
+                        <span class="icon"><i class="fa fa-paper-plane"></i></span>
+                        <span class="caption">kcmit.edu.np </span>
+                    </li>
+                    <li>
+                        <span class="icon"><i class="fa fa-phone"></i></span>
+                        <span class="caption">+977-1-4479939</span>
+                    </li>
+                    
+                    <li>
+                        <span class="icon"></span>
+                        <span class="caption">Copyright &copy; All right reserve</span>
+                    </li>
+                    
+                </ul>
+                        
+            </div>
+        </footer>
---
+++ index.php
@@ -0,0 +1,137 @@
+<?php
+	$conn= new mysqli('localhost','root','','book_store_db')or die("Could not connect to mysql".mysqli_error($con));
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Home Page</title>
+		<link rel='stylesheet' href='CSS/header.css' type='text/css'/>
+		<link rel='stylesheet' href='CSS/footer.css' type='text/css'/>
+		<link rel='stylesheet' href='CSS/index.css' type='text/css'/>
+		<link rel="stylesheet" href="CSS/fontAwesome/css/font-awesome.css" type="text/css"/>
+		<style>
+		    #cat-list li{
+		        cursor: pointer;
+		    }
+		    #cat-list li:hover {
+		        color: white;
+		        background: #007bff8f;
+		    }
+		    .prod-item p{
+		        margin: unset;
+		    }
+		    .bid-tag {
+		    	position: absolute;
+		    	right: .5em;
+			}
+		</style>
+	</head>
+	<body>
+		<?php 
+			$cid = isset($_GET['category_id']) ? $_GET['category_id'] : 0;
+		?>
+		<div class="inner-header-wrapper" style="">
+			<div class="logo">
+				<a href="#"><img src="assets/img/logo.png" alt="logo"/></a>
+			</div>
+			<div class="inner-header">
+				<div class="title">
+					<a href="">Online Book Store</a>
+				</div>
+				<div class="nav-menu">
+					<ol>
+						<li><a href="fee_info.php">Home</a></li>
+						<li><a href="feedetails.php">Cart</a></li>
+						<li><a href="Database/signout_db.php">About</a></li>
+						<li><a href="">Login</a></li>
+					</ol>
+				</div>
+			</div>
+		</div>
+		<div class="contain-fluid">
+    		<div class="col-lg-12">
+        		<div class="row">
+            		<div class="col-md-3">
+                		<div class="card">
+                    		<div class="card-header">Categories</div>
+                   				 <div class="card-body">
+                        			<ul class='list-group' id='cat-list'>
+                            			<li class='list-group-item' data-id='all' data-href="index.php?page=home&category_id=all">All</li>
+                            			<?php
+                               				$cat = $conn->query("SELECT * FROM categories order by name asc");
+                                			while($row=$cat->fetch_assoc()):
+                                    			$cat_arr[$row['id']] = $row['name'];
+                             			?>
+                            			<li class='list-group-item' data-id='<?php echo $row['id'] ?>' data-href="index.php?page=home&category_id=<?php echo $row['id'] ?>"><?php echo ucwords($row['name']) ?></li>
+
+                            			<?php endwhile; ?>
+                        			</ul>
+
+                    			</div>
+                		</div>
+            		</div>
+            		<div class="col-md-9">
+                		<div class="card">
+                    		<div class="card-body">
+                        		<div class="row">
+                            		<?php
+                                		$where = "";
+                                		if($cid > 0){
+                                    		$where  = " where CONCAT('[',REPLACE(category_ids,',','],['),']') LIKE '%[".$cid."]%'  ";
+                                		}
+                                		$books = $conn->query("SELECT * from books $where order by title asc");
+                                		if($books->num_rows <= 0){
+                                    		echo "<center><h4><i>No Available Product.</i></h4></center>";
+                               			} 
+                                		while($row=$books->fetch_assoc()):
+                             		?>
+                             		<div class="col-sm-4">
+                                 		<div class="card">
+                                    		<div class="float-right align-top bid-tag">
+                                         		<span class="badge badge-pill badge-primary text-white"><i class="fa fa-tag"></i> <?php echo number_format($row['price']) ?></span>
+                                     		</div>
+                                     		<div class="card-img-top d-flex justify-content-center" style="max-height: 30vh;overflow: hidden">
+                                     			<img class="img-fluid" src="admin/assets/uploads/<?php echo $row['image_path'] ?>" alt="Card image cap">
+                                       
+                                     		</div>
+                                      		<div class="float-right align-top d-flex">
+                                     		</div>
+                                     		<div class="card-body prod-item">
+                                         		<p>Title: <?php echo $row['title'] ?></p>
+                                         		<p>Author: <?php echo $row['author'] ?></p>
+                                         		<p>
+                                            		<small>
+                                          				<?php 
+                                          					$cats = '';
+                                          					$cat = explode(',', $row['category_ids']);
+                                          					foreach ($cat as $key => $value) {
+	                                            				if(!empty($cats)){
+	                                              					$cats .=", ";
+	                                            				}
+	                                            				if(isset($cat_arr[$value])){
+	                                              					$cats .= $cat_arr[$value];
+	                                            				}
+                                          					}
+                                          					echo $cats;
+                                          				?>
+                                            
+                                            		</small>
+                                          		</p>
+                                         		<p class="truncate"><?php echo $row['description'] ?></p>
+                                        		<button class="btn btn-primary btn-sm view_prod" type="button" data-id="<?php echo $row['id'] ?>"> View</button>
+                                     		</div>
+                                 		</div>
+                             		</div>
+                            		<?php endwhile; ?>
+                            	</div>
+                    		</div>
+                		</div>
+            		</div>
+        		</div>
+    		</div>
+    	</div>	
+		<?php 
+			include 'footer.php';
+		?>
+	</body>
+</html>
