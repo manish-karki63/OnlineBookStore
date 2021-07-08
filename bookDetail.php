cc74ebae9f30413f1430434a2c4c05c600e89b68
manish-karki63 <manish.karki33@gmail.com>
2021-07-08 15:34:15 +0545

    upto detail css

--- bookDetail.php
+++ bookDetail.php
@@ -3,6 +3,7 @@
 		<title>Book Detail</title>
 		<link rel='stylesheet' href='css/header.css' type='text/css'/>
 		<link rel='stylesheet' href='css/footer.css' type='text/css'/>
+		<link rel="stylesheet" type="text/css" href="css/bookDetail.css">
 		<link rel="stylesheet" href="css/fontAwesome/css/font-awesome.css" type="text/css"/>
 		
 	</head>
@@ -27,17 +28,17 @@
 		</div>
 
 		<!--Book Detail-->
-		<div>
-			<div>
+		<div class="book-info-section">
+			<div class="image">
 				<img src="assets/img/1604631960_python_book.JPG" alt="img"/>
 			</div>
-			<div>
-				<ul>
-					<li><span>Title</span> Core Java Volume I – Fundamentals</li>
-					<li><span>Author</span>Cay S. Horstmann</li>
-					<li><span>Category</span>Educational, Programming</li>
-					<li><span>Price</span>2500</li>
-					<li><span>Description</span>
+			<div class="book-container">
+				<ul class="book-info">
+					<li><span>Title:</span> Core Java Volume I – Fundamentals</li>
+					<li><span>Author:</span>Cay S. Horstmann</li>
+					<li><span>Category:</span>Educational, Programming</li>
+					<li><span>Price:</span>2500</li>
+					<li><span>Description:</span>
 						<p>Core Java Volume I – Fundamentals is a Java reference book (Best book for Java)that offers a detailed explanation of various features of Core Java, including exception handling, interfaces, and lambda expressions. Significant highlights of the book include simple language, conciseness, and detailed examples. The latest edition of the Core Java Volume I – Fundamentals comprehensively updated for covering Java SE 9, 10 & 11. The book helps Java programmers develop an ability to write highly robust and maintainable code.</p>
 					</li>
 				</ul>
